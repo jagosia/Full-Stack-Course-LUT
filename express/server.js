9452fb5e11a81acaea37c928e981aa0f1c4d0c42
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-20 20:19:32 +0300

    cleaning posts file using controller

--- express/server.js
+++ express/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 import posts from './routes/posts.js';
 
 const port = process.env.PORT || 8000;
@@ -18,5 +20,9 @@ app.use(logger);
 
 // Routes
 app.use('/api/posts', posts);
+
+// Error handler
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
