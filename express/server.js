3817f20c2c8baabdf06bec20885bdccfc77f72c6
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-20 18:27:37 +0300

    basic express api server

--- express/server.js
+++ express/server.js
@@ -1,4 +1,34 @@
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
 const app = express();
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+let posts = [
+    { id: 1, title: 'Post One' },
+    { id: 2, title: 'Post Two' },
+    { id: 3, title: 'Post Three' },
+];
+
+app.get('/api/posts', (req, res) => {
+    const limit = parseInt(req.query.limit);
+    if (!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit));
+    }
+    res.status(200).json(posts);
+});
+
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+
+    if (!post) {
+        return res.status(404).json({ msg: `A post with the id of ${id} was not found` });
+    }
+
+    res.status(200).json(post);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
