193f42f58a0fa84962f75ac63dfc2a324a9bd842
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-20 20:34:44 +0300

    adding simple frontend

--- express/server.js
+++ express/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 import posts from './routes/posts.js';
-
 const port = process.env.PORT || 8000;
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,10 +17,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Logger middleware
 app.use(logger);
 
 // setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/posts', posts);
