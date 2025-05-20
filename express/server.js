7b76055009aa4a1c69e1bdada167217792d1730e
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-20 18:41:30 +0300

    cleaning the server.js using posts.js

--- express/server.js
+++ express/server.js
@@ -1,34 +1,15 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+// setup static folder (optional)
 // app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-    { id: 1, title: 'Post One' },
-    { id: 2, title: 'Post Two' },
-    { id: 3, title: 'Post Three' },
-];
-
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit);
-    if (!isNaN(limit) && limit > 0) {
-        return res.status(200).json(posts.slice(0, limit));
-    }
-    res.status(200).json(posts);
-});
-
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find(post => post.id === id);
-
-    if (!post) {
-        return res.status(404).json({ msg: `A post with the id of ${id} was not found` });
-    }
-
-    res.status(200).json(post);
-});
+// Routes
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
