949bda088ed20ca8bf4123f8be92605fc98edbbc
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-21 10:52:07 +0300

    adding tasks

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask'
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -23,6 +24,12 @@ const App = () => {
       reminder: false,
     },
   ]);
+  // Add Task
+  const addTask = async (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -41,6 +48,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
