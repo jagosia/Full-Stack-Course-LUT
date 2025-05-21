1440992d7fcc59e6fe2c80cb8e373b13b9ba6fd8
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-21 09:20:16 +0300

    simple examples

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,10 +1,20 @@
-function App() {
-  return (
-    <div className='container'>
-      <h1>Hello From React</h1>
-      <h2>Hello</h2>
-    </div>
-  )
+import React from "react"
+import Header from "./components/Header"
+
+// function App() {
+//   return (
+//     <div className='container'>
+//       <Header />
+//     </div>
+//   )
+// }
+
+// export default App
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>
+  }
 }
 
 export default App
