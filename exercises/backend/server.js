87e404e28382dff072e258f5a37e1abe0e26ae35
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-21 16:05:12 +0300

    db connection

--- exercises/backend/server.js
+++ exercises/backend/server.js
@@ -1,15 +1,21 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware');
+const colors = require('colors')
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' })
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+console.log('MONGO_URI is:', process.env.MONGO_URI)
+
+connectDB()
 
 const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/goals', require('./routes/goalRoutes'))
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
